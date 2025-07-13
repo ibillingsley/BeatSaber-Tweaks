af3e8dac31b05f09a5a64dffe30fd174b6afd1e8
Isaiah Billingsley <iza.billingsley@gmail.com>
2025-07-13 16:58:02 -0400

    Initial commit

---
+++ Patches/HookFilterRefresh.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+using System.Linq;
+using HarmonyLib;
+
+namespace BugFix.Patches
+{
+	// Clear BetterSongList filter when selecting a BeatSaberPlus song request
+
+	[HarmonyPatch(typeof(LevelSearchViewController), nameof(LevelSearchViewController.Refresh), new[] { typeof(LevelFilter) }, new[] { ArgumentType.Ref })]
+	static class HookFilterRefresh
+	{
+		static void Prefix(LevelSearchViewController __instance)
+		{
+			// From https://github.com/kinsi55/BeatSaber_BetterSongList
+			var x = new StackTrace().GetFrames()?.DefaultIfEmpty(null).ElementAtOrDefault(2)?.GetMethod().DeclaringType?.Assembly.GetName().Name;
+			if (x == "Main")
+				return;
+
+			__instance.ResetAllFilterSettings(false);
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using IPA;
+using IPA.Loader;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
+
+namespace BugFix
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    internal class Plugin
+    {
+        internal static IPALogger Log { get; private set; }
+        internal static Harmony Harmony { get; private set; }
+
+        // Methods with [Init] are called when the plugin is first loaded by IPA.
+        // All the parameters are provided by IPA and are optional.
+        // The constructor is called before any method with [Init]. Only use [Init] with one constructor.
+
+        [Init]
+        public Plugin(IPALogger ipaLogger, PluginMetadata pluginMetadata)
+        {
+            Log = ipaLogger;
+
+            Log.Info($"{pluginMetadata.Name} {pluginMetadata.HVersion} initialized.");
+        }
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            Log.Debug("OnApplicationStart");
+            Harmony = new Harmony("iza.BugFix");
+            Harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            Log.Debug("OnApplicationQuit");
+            Harmony.UnpatchSelf();
+        }
+    }
+}
